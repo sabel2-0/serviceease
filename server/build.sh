5fdcf785c1b82ceadee8876d23017bd9682616b8
Mark Ivan Sumalinog <markivan.note@gmail.com>
2025-12-13 12:00:41 +0800

    Move build script to server directory to match Render root directory

--- server/build.sh
+++ server/build.sh
@@ -2,9 +2,9 @@
 # Render build script for ServiceEase
 
 echo "Installing Python dependencies..."
-pip install -r server/scripts/requirements.txt
+pip install -r scripts/requirements.txt
 
 echo "Installing Node.js dependencies..."
-cd server && yarn
+yarn
 
 echo "Build completed successfully!"
